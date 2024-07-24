4a0d8bc3fb9e136338eb10f1dd1eeff62e41e9a2
Joseb Beridze <148484902+bero20005@users.noreply.github.com>
2024-07-24 19:18:24 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS, JSX, TS, TSX files in src directory
+  ],
+  theme: {
+    extend: {
+      // Extend the default theme here
+    },
+  },
+  plugins: [
+    // Add Tailwind CSS plugins here
+  ],
+};
